a8590a30dd7c394d2febf72e583a7ad058091231
kaori kohama <kkaori131415@gmail.com>
2020-06-27 15:11:34 +0200

    added image and text to profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,14 +1,43 @@
 <template>
   <div class="profile">
-    <h1>This is an about page</h1>
+    <div class="profile-top">
+      <img :src="images.myPhoto" class="myPhoto">
+      <h2>
+        Kaori Kohama<br>
+        I am a software developer.
+      </h2>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Profile',
+  data(){
+    return {
+      images: {
+        myPhoto: require("../assets/profile-image2.png")
+      }
+    }
+  },
   components: {
     
   }
 }
 </script>
+
+<style>
+
+  .profile{
+    text-align: center;
+    align-items: center;
+  }
+  .profile-top{
+    display: flex;
+  }
+
+  .myPhoto{
+    height: 150px;
+    width: 150px;
+  }
+</style>
